81f97a58fc979286b381dbf83a623dc4ebda92bc
HELLOSJ00 <skku03062@g.skku.edu>
2024-03-11 10:58:04 +0900

    스크립트 코드 추가

--- src/app/(pages)/MainLayout.js
+++ src/app/(pages)/MainLayout.js
@@ -8,6 +8,7 @@ const queryClient = new QueryClient();
 export default function MainLayout({children}){
     return(
         <QueryClientProvider client={queryClient}>
+            <script defer src="https://cdn.swygbro.com/public/widget/swyg-widget.js"></script>
             <Header/>
             {children}
         </QueryClientProvider>
