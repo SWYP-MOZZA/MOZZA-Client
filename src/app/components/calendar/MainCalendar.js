ed4acea8fbf4c443ea26bf6fdcbf7d264ad07f84
hyewon <wonny2327@gamil.com>
2024-03-23 08:48:10 +0900

    :bug: 캘린더 css useclient 수정

--- src/app/components/calendar/MainCalendar.js
+++ src/app/components/calendar/MainCalendar.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import { format, isWithinInterval, parseISO, startOfDay, endOfDay } from 'date-fns';
